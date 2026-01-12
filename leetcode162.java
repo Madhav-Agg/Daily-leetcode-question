86e3ed20edeef4aaadc8641b0907194377f06fd4
Madhav Aggarwal <aggarwalmadhav44@gmail.com>
2026-01-12 19:04:18 +0530

    Add files via upload

---
+++ leetcode162.java
@@ -0,0 +1,22 @@
+package Searching;
+
+public class leetcode162 {
+    public static void main(String[] args) {
+        int []arr={1,2,3,1};
+        int ans= findPeakElement(arr);
+        System.out.println(ans);
+    }
+    static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]>nums[mid+1]){
+                end=mid;
+            }else{
+                start=mid+1;
+            }
+        }
+        return start; //return end also works
+}
+}
